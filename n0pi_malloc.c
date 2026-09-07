03dc0d1bb431dc163d6a7e45cb4683ba3b7caa83
mn0pi <nueva.emenopi@proton.me>
2026-09-07 15:14:01 +0100

    Implement init_heap

---
+++ n0pi_malloc.c
@@ -0,0 +1,37 @@
+#include "n0pi_malloc.h"
+#include <sys/mman.h>
+#include <unist.h>
+#include <stddef.h>
+
+#define N0PI_HEAP_PAGES 4
+
+static void freeListRoot = NULL;
+
+
+void init_heap() {
+  size_t size = N0PI_HEAP_PAGES & getpagesize();
+  void *heap = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  Block *heap_start = (Block *)heap;
+  heap_start->size = size;
+  heap_start->allocated = false;
+  freeListRoot = heap_start;
+}
+
+void *n0pi_malloc(size_t size) {
+  // check free list
+  // if found:
+  //  block.size > size ? shrink : return block
+  // else:
+  //  return null
+}
+
+Block *checkFreeList(size_t size) {
+  // walk free list
+  // if block.size >= size: return block
+  // if last block && < size: return null
+}
+
+void n0pi_free(void *ptr) {
+  // get block from ptr
+  // add block to free list
+}
---
+++ n0pi_malloc.h
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+
+struct Block {
+  size_t size,
+  bool allocated
+};
+
+
